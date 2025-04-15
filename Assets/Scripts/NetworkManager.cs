036e6b22899306d33433bc1e40239e671fecc2c6
karanveer <jaipalveer@gmail.com>
2025-04-15 12:20:34 +0530

    Scene Transition impleemented Lobby -> Create Random Session [PlayScene in our case].

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -4,12 +4,15 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 // this one acts as a central network manager from creating rooms/sessions to holding them up in session.
 // Implemented and derived methods by and from INetworkRunnerCallbacks, are self explanatory, if you need me to explain you something in detail feel free to ask.
 public class NetworkManager : MonoBehaviour, INetworkRunnerCallbacks
 {
     public static NetworkRunner runnerInstance; // to store our runner for that instance, the names are going to be pretty self explanatory to be honest.
-
+    public string lobbyName = "default";
+    public TMPro.TMP_InputField roomNumberInputField;
+    public string playSceneName;
     private void Awake()
     {
 
@@ -18,6 +21,59 @@ public class NetworkManager : MonoBehaviour, INetworkRunnerCallbacks
         {
             runnerInstance = gameObject.AddComponent<NetworkRunner>();
         }
+    }
+    public void CreateRandomSession()
+    {
+        // int randomInt = UnityEngine.Random.Range(1000, 9999);
+        int roomNumber = 0; // default fallback value
+
+        // We Check if the input field is empty. If not, try parsing its content.
+        if (!string.IsNullOrEmpty(roomNumberInputField.text))
+        {
+            if (!int.TryParse(roomNumberInputField.text, out roomNumber))
+            {
+                Debug.LogError("Invalid room number input!");
+                return;
+            }
+
+        }
+        else //field was empty/null
+        {
+            Debug.Log("Field was empty, defaulted to Room Number = 0");
+        }
+
+        int randomInt = roomNumber; // now we input it from UI itself, so use same room number to join your friends.
+
+        string randomSessionName = "Room : " + randomInt.ToString();
+        NetworkManager.runnerInstance.StartGame(new StartGameArgs()
+        {
+            Scene = SceneRef.FromIndex(GetSceneIndex(playSceneName)),
+            SessionName = randomSessionName,
+            GameMode = GameMode.Shared,
+        }); ;
+    }
+    public int GetSceneIndex(string sceneName)
+    {
+        for (int i = 0; i < SceneManager.sceneCountInBuildSettings; i++)
+        {
+            // We get the scene path
+            string scenePath = SceneUtility.GetScenePathByBuildIndex(i);
+            // Extracting the name of the scene from the path
+            string name = System.IO.Path.GetFileNameWithoutExtension(scenePath);
+            // Checking if the names match
+            if (name == sceneName)
+            {
+                // If matched, return
+                return i;
+            }
+        }
+        // If the scene name is not found, return -1 (error code)
+        return -1;
+    }
+    private void Start()
+    {
+        runnerInstance.JoinSessionLobby(SessionLobby.Shared, lobbyName);
+
     }
     public void OnConnectedToServer(NetworkRunner runner)
     {
@@ -114,15 +170,4 @@ public class NetworkManager : MonoBehaviour, INetworkRunnerCallbacks
        
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
